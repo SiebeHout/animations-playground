72359e712c76d968791ab9947f72bedc42bcef77
Siebe Burgerhout <siebe.burgerhout@icloud.com>
2025-02-12 10:14:29 +0100

    inital+arrow

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,35 @@
 import type { Config } from "tailwindcss";
+import * as tailwindAnimate from "tailwindcss-animate";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
+      },
+    },
     extend: {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
   },
-  plugins: [],
+  plugins: [tailwindAnimate],
 } satisfies Config;
